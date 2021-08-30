07b53d1858307e21ab27669b604d78169ab63d53
anastefan9 <ana.stefan1000@gmail.com>
2021-08-30 13:51:21 +0300

    Third commit

--- script.js
+++ script.js
@@ -1,68 +1,60 @@
-document.body.onload = addElement;
-var nrCell = 1, status = 0;
+document.body.onload = createCell;
+var nrCell = 0, status = 0;
 var message = document.getElementById("message");
 var button = document.getElementById("button");
 
-function addElement() {
+function createCell() {
 	for (var i = 1; i <= 3; ++i) {
 		for (var j = 1; j <= 3; ++j) {
 			const object = document.getElementById("container");
 			const newDiv = document.createElement("div");
-			newDiv.onclick = function() {changeText(newDiv)};
+			newDiv.onclick = function() {printText(newDiv)};
 			newDiv.setAttribute("id", "C" + i + j);
 			container.appendChild(newDiv);
 		}
 	}
 }
-function changeText(div) {
+function printText(div) { 
 	if (status == 0) {
-		var row = (div.id).charAt(1);
-		var column = (div.id).charAt(2);
-
 		if (div.innerHTML == "") {
-			div.appendChild(text(nrCell));
+			div.appendChild(text(nrCell)); 
 			++nrCell;
-		}
-		if (winner(row, column, div) == 0 && nrCell > 4) {
-			message.innerHTML = div.innerHTML + " a castigat!";
-			status = 1;
-			button.style.display = "block";
-		} else if (nrCell == 10) {
-			message.innerHTML = "Remiza!";
-			button.style.display = "block";
+			getGameResult(div); 
 		}
 	} 
 }
-function text(number) {
+function text(number) { 
 	if (number % 2 == 0) {
-		var newText = document.createTextNode("0");
-	} else {
 		var newText = document.createTextNode("X");
+	} else {
+		var newText = document.createTextNode("0");
 	}
 	return newText;
 }
-function winner(rowNr, colNr, text) {
+function checkWinner(cellText) { 
+	var row = (cellText.id).charAt(1);
+	var col = (cellText.id).charAt(2);
 	var winRow = 0, winColumn = 0, winMainDiag = 0, winSecDiag = 0;
 	for (var j = 1; j <= 3; ++j) {
-		var rowDiv = document.getElementById("C" + rowNr + j);
-		if (rowDiv.innerHTML != text.innerHTML) {
+		var rowDiv = document.getElementById("C" + row + j);
+		if (rowDiv.innerHTML != cellText.innerHTML) {
 			winRow = 1;
 		} 
-		var columnDiv = document.getElementById("C" + j + colNr); 
-		if (columnDiv.innerHTML != text.innerHTML) {
+		var columnDiv = document.getElementById("C" + j + col); 
+		if (columnDiv.innerHTML != cellText.innerHTML) {
 			winColumn = 1;
 		}
-		if (rowNr == colNr) { 
+		if (row == col) { 
 			var divMainDiag = document.getElementById("C" + j + j);
-			if (divMainDiag.innerHTML != text.innerHTML) {
+			if (divMainDiag.innerHTML != cellText.innerHTML) {
 				winMainDiag = 1;
 			}
 		} else {
 			winMainDiag = 1;
 		}
-		if (colNr == 3 - rowNr + 1) { 
+		if (col == 3 - row + 1) { 
 			var divSecDiag = document.getElementById("C" + j + (3 - j + 1));
-			if (divSecDiag.innerHTML != text.innerHTML) {
+			if (divSecDiag.innerHTML != cellText.innerHTML) {
 				winSecDiag = 1;
 			}
 		} else {
@@ -73,4 +65,21 @@ function winner(rowNr, colNr, text) {
 		return 0;
 	} 
 	return 1;
+}
+function getGameResult(cell) {
+	if (checkWinner(cell) == 0 && nrCell > 4) { 
+		printResult(cell);
+	} else if (nrCell == 9) { 
+		printResult("1");     
+	} 
+}
+function printResult(variable) { 
+	if (variable == 1) {
+		message.innerHTML = "Remiza!";
+		button.style.display = "block";
+	} else {
+		message.innerHTML = variable.innerHTML + " a castigat!";
+		status = 1;
+		button.style.display = "block";
+	}
 }
